8d1762b3d3dd658383c4221e1d9f51a6a09c1276
hal-cha-n <halstroemeria.tyan@gmail.com>
2022-05-30 01:06:06 +0900

    はるちゃんの配信予定5件を取得するように修正

--- main.go
+++ main.go
@@ -28,34 +28,18 @@ func newYoutubeService(client *http.Client) *youtube.Service {
 
 func printChannelInfo(channelID string) {
 	service := newYoutubeService(newClient())
-	call := service.Channels.List([]string{"snippet", "contentDetails", "statistics"}).
-		Id(channelID).
-		MaxResults(1)
+	call := service.Search.List([]string{"snippet", "id"}).
+		ChannelId(channelID).
+		Order("date")
+
 	response, err := call.Do()
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
-	item := response.Items[0]
 
-	id := item.Id
-	name := item.Snippet.Title
-	description := item.Snippet.Description
-	thumbnailURL := item.Snippet.Thumbnails.High.Url
-	playlistID := item.ContentDetails.RelatedPlaylists.Uploads
-	viewCount := item.Statistics.ViewCount
-	subscriberCount := item.Statistics.SubscriberCount
-	videoCount := item.Statistics.VideoCount
-
-	fmt.Printf("channel id: %v\n\nチャンネル名: \n%v\n\n説明: %v\n\nサムネイルURL: %v\n\nplaylist id: %v\n\n総再生回数: %v\n\nチャンネル登録者数: %v\n\n動画数: %v\n",
-		id,
-		name,
-		description,
-		thumbnailURL,
-		playlistID,
-		viewCount,
-		subscriberCount,
-		videoCount,
-	)
+	for i, v := range response.Items {
+		fmt.Printf("%d. %+v: %+v\n", i, v.Snippet.Title, v.Snippet.PublishedAt)
+	}
 }
 
 func main() {
