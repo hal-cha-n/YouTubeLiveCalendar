45ab9c4027ab2a7a8501ba34743c0bd6f36fee52
hal-cha-n <halstroemeria.tyan@gmail.com>
2022-05-30 01:51:50 +0900

    カレンダーへの登録テスト追加

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/googleapi/transport"
 	"google.golang.org/api/youtube/v3"
 )
@@ -19,6 +21,16 @@ func newClient() *http.Client {
 
 func newYoutubeService(client *http.Client) *youtube.Service {
 	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatalf("Unable to create YouTube service: %v", err)
+	}
+
+	return service
+}
+
+func newCalenderService() *calendar.Service {
+	ctx := context.Background()
+	service, err := calendar.NewService(ctx)
 	if err != nil {
 		log.Fatalf("Unable to create YouTube service: %v", err)
 	}
@@ -41,6 +53,7 @@ func printChannelInfo(channelID string) {
 	for i, v := range response.Items {
 		fmt.Printf("%d. %+v\n", i, v.Snippet.Title)
 		youtube_video_details(v.Id.VideoId)
+		createEvent()
 	}
 }
 
@@ -55,6 +68,32 @@ func youtube_video_details(videoId string) {
 	}
 
 	fmt.Printf("配信予定時刻：%+v", response.Items[0].LiveStreamingDetails.ScheduledStartTime)
+}
+
+func createEvent() {
+
+	event := &calendar.Event{
+		Summary:     "テスト",
+		Description: "テスト説明",
+		Start: &calendar.EventDateTime{
+			DateTime: "2022-06-04T13:00:00Z",
+			TimeZone: "Europe/London",
+		},
+		End: &calendar.EventDateTime{
+			DateTime: "2022-06-04T14:00:00Z",
+			TimeZone: "Europe/London",
+		},
+	}
+
+	service := newCalenderService()
+	call := service.Events.Insert("m323k3iij27jdlq1m0qvoppldo@group.calendar.google.com", event)
+
+	_, err := call.Do()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	fmt.Println("success")
 }
 
 func main() {
